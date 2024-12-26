f98447b5330df91190c50905efa2478a28899793
jishou <144637328+KJiShou@users.noreply.github.com>
2024-12-26 16:50:04 +0800

    update auto format

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 // import { useState } from 'react'
-import { Button } from "@arco-design/web-react";
-import "@arco-design/web-react/dist/css/arco.css";
+import { Button } from '@arco-design/web-react';
+import '@arco-design/web-react/dist/css/arco.css';
 
 function App() {
-
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <Button type="primary">Hello Arco</Button>
-      <Button type="primary">Hello Arco</Button>
-        <h1 className="text-4xl font-bold text-blue-600">
-          Welcome to Home Page
-        </h1>
-        <p className="text-lg mt-4 text-gray-700">
-          This page uses Tailwind CSS!
-        </p>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+                <Button type="primary">Hello Arco</Button>
+                <Button type="primary" onClick={() => {}}>
+                    Hello Arco
+                </Button>
+                <h1 className="text-4xl font-bold text-blue-600">
+                    Welcome to Home Page
+                </h1>
+                <p className="text-lg mt-4 text-gray-700">
+                    This page uses Tailwind CSS!
+                </p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
